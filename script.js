d6f6156c6a733024727ae24412629751e4048454
shubham2o <shubhamsharma.ha@gmail.com>
2022-08-23 23:59:36 +0530

    Code - script . js (working) of the project added

---
+++ script.js
@@ -0,0 +1,83 @@
+const grid = document.querySelector('.grid');
+const blockWidth = 100;
+const blockHeight = 20;
+const boardWidth = 560;
+
+const userStart = [230, 20];
+let currentPosition = userStart;
+
+// Create Block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [xAxis + blockWidth, yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRRight = [xAxis + blockWidth, yAxis + blockHeight];
+    }
+}
+
+// Create new blocks
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+];
+
+console.log(blocks[0]);
+
+// Draw all my block 
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] + 'px';
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+        grid.appendChild(block);
+    }
+}
+
+addBlocks();
+
+// Add user
+const user = document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
+
+// Draw the user
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px';
+    user.style.bottom = currentPosition[1] + 'px';
+}
+
+// Move user
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10;
+                drawUser();
+            }
+            break;
+        case 'ArrowRight':
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10;
+                drawUser();
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser);
